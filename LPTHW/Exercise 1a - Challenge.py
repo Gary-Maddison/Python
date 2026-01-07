c68d54635466c9af7f5ba95dc813bfabea127984
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-07 15:01:37 +0000

    New file added

---
+++ LPTHW/Exercise 1a - Challenge.py
@@ -0,0 +1,8 @@
+print("My name is Gary Maddison and I live in Oldham")
+
+#The line below will not print as the string has not been closed. Its missing the '
+#print('This line has an error)
+# I want to highlight the quatation marks so I need to use ''
+# However as I am using ' in the string I need to enter \ to escape the string
+
+print('He said, "It\'s a beautiful day!"')
