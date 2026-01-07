f7db1011f14b26f1f5ce32654de3a41561fca310
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-07 14:46:47 +0000

    Added my first project: LPTHW

---
+++ LPTHW/Exercise 1 - A Good First Program.py
@@ -0,0 +1,14 @@
+# This will print the words contained within the strings ""
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this")
+print("This is fun")
+
+# Instead of "" you can also use ''
+
+print('Yay! Printing')
+print("I'd much rather you 'not'.")
+
+# If you want to use "" in your code use '' instead
+print('I "said" rather you not')
