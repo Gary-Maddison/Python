0def7b1bfc8a5a00c15a49c30506c739d1530c67
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-08 08:31:48 +0000

    Message Sent

---
+++ PY4E/1. Variables, Expressions and Statements.py
@@ -0,0 +1,14 @@
+# The left side is called the variable - hrs
+# A statement is the = as we are asking the computer to store information.
+# The right side is called the expression = input("Enter Hours":)
+# The right side is done first before the left sides entry is submitted.
+# A statement is a command to perform an action - "Eat a Red Apple"
+
+
+hrs = input("Enter Hours:")
+rate = input("Enter Hourly Rate:")
+
+pay = float(hrs) * float(rate)
+
+print("Pay:", pay)
+
