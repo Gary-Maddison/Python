8edb1e5dc426f447ca3a56008f8f7a49c7d16c4a
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-08 09:49:27 +0000

    Message Sent

---
+++ Projects/Challenge 2 - The ID Badge Creator.py
@@ -0,0 +1,13 @@
+print("ID Badge Creator")
+print()
+first_name = input("First Name: ")
+last_name = input("Last Name: ")
+job_title = input("Job Title: ")
+fav_food = input("Favourite Food: ")
+print()
+print("--- OFFICIAL BADGE ---")
+print("Name: ", last_name, ", ", first_name, sep="") # The sep commend removes the automatic space that python automatically places. 
+print("Job:", job_title)
+print("Snack:", fav_food)
+print("----------------------")
+
