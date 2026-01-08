4519474b993c0d51bbf59d73410b03eb027c2f93
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-08 09:19:55 +0000

    Message Sent

---
+++ Projects/Challenge 1 - The Personal Greeter.py
@@ -0,0 +1,5 @@
+print("Hello, its nice to meet you")
+name = input("What is your Name?: ")
+fav_col = input("What is your favourite colour?: ")
+print() # This creates a blank line
+print("Hello", name, ",I also think", fav_col, "is a great choice!")
