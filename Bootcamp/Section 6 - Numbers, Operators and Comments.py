57417c18ad0b59f17c62b341fd6df9064aaf991c
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-14 14:16:04 +0000

    Message Sent

---
+++ Bootcamp/Section 6 - Numbers, Operators and Comments.py
@@ -0,0 +1,32 @@
+print(34) # This is a integer
+print(type(34)) # Here the function type tells us what class we have
+
+print(2.33) # This is a float 
+print(type(2.33)) # This tells us we have a float class
+
+print(10 + 22.33) # If you add an integer and a float python will give you a float
+print(type(10 + 22.33)) # This is telling us we haver a float class
+
+# Common Operators
+
+# Addition +
+# Subtraction -
+# Multiplication *
+# Division /
+# Exponentional **
+# Modulo %
+# Integer Division //
+
+print(10/2) # A division always returns back a float, even when dividing ints
+print(2 ** 3) # This is 2 to the 3 power giving us 8
+print(10 % 3) # Here we are using modulo so it gives us the remiander giving us 1
+print(10 // 3) # This operation will round down the division giving us 3 instead of 3.333
+
+# Order of Operations
+
+# P - Parenthesis ()
+# E - Exponents ** (So 2 to power of 3 = 2 * 2 * 2 = 8)
+# M - Multiplication *
+# D - Division /
+# A - Addition +
+# S - Subtraction -
