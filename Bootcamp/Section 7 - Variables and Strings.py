9b29f6d5b8b838c85ca5e9c83dc4e0cdb00c4e70
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-15 09:48:36 +0000

    Message Sent

---
+++ Bootcamp/Section 7 - Variables and Strings.py
@@ -0,0 +1,35 @@
+tips_jar = 100  # Here we have a variable called tips_jar and inside we have put in 100
+print(tips_jar) #  Here we are printing the value of what is inside the jar
+
+tips_jar = "Nothing" # Here we are putting text inside the jar so we to use " " to show this is a string
+print(tips_jar) # This now print the word Nothing
+
+# Naming Restrictions
+
+# Variables must start with a letter or an underscore _
+# Variables are case-sensitive
+# Some names are not allowed as they are used in the python language - print, input
+
+# Data Types
+
+# bool - True or False
+# int - 1, 2, 3
+# str - Unicode characters, eg. "Gary or "hghgehd"
+# list - An ordered sequence of value, eg. [1, 2, 3] or ["a", "b", "c"]
+# dict - A collection of key values: values, eg. {"first_name": "Gary", "last_name": "Maddison"}
+
+# Special value called None
+
+no_children = None # This repesents nothingness
+print(type(no_children))# This is a NoneType
+
+# Double and single quotes
+
+print("Gary Maddison") # What you open your string with you close with
+print('Gary Maddison')
+print('Gary said "Hello"') # Here I want show " " within the string so I use single quotes
+
+# String Escape Characters
+
+
+
