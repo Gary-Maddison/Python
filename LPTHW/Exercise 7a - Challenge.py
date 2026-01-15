0271965bea3fec43df3f4f6ca1b1a82f1c3aa44e
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-15 09:51:39 +0000

    Message SEnt

--- LPTHW/Exercise 7a - Challenge.py
+++ LPTHW/Exercise 7a - Challenge.py
@@ -1,3 +1,3 @@
 line_break = "=" * 10
-print(line_break, end=' ')  # Print a line of '=' characters 10 times without a newline
+print(line_break, end=(" "))  # Print a line of '=' characters 10 times without a newline
 print("100% Complete")
