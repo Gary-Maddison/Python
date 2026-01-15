0b83c7da1ce50f839ffc1ea62d29ea79d33d449c
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-15 14:59:21 +0000

    Message Sent

---
+++ Bootcamp/Section 8 - Boolean and Conditional Logic.py
@@ -0,0 +1,44 @@
+
+# Conditional Statements
+
+name = "Jack Maddison"
+if name == "Gary Maddison":  # The : says there is going to be indednt below.
+    print("Gary Maddsion")
+elif name == "Leasa Maddsion": # You can have more than one elif
+    print("Leasa Maddison")
+elif name == "Ben Maddison":
+    print("Ben Maddison")
+elif name == "Liam Maddison":
+    print("Liam Maddison")
+else:
+    print("Jack Maddson")
+
+# Truthiness and Falsey
+
+x = 1
+x is 1 # True
+x is 0 # False
+if 0:
+    print("False") # This will not print because its False
+if 1:
+    print("True") # This will print because its True
+
+print(" ") # This is classed as False
+
+# Comparison Operators
+
+# 1 == 1 - Is True
+# a != b = False ( A is  ot equal to b)
+# 1 > b = False
+# 1 < 2 = True
+# 1 >= 1 = True (Greater or equal than)
+# 1 <= 1 = True (Less than or equal to)
+
+# Logical Operators
+
+# and (Both a and b both have to be true)
+# or (either a or b need to be true)
+# not (As to be the opposite of something to be True) 
+
+# is vs. ==
+# == checks to to see if they are the same but (is) checks if they are stored in the same place in memory
