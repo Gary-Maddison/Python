f5d1f92eb2746483dc541bf264b54384d3584410
Gary-Maddison <garyfmaddison@gmail.com>
2026-01-19 09:27:41 +0000

    Message SEnt

---
+++ LPTHW/Exercise 9 - Multi-Line Strings.py
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"  # \n means new line
+
+print("Here are the days: ", days)  # Here we are pulling up the days variable
+print("Here are the months", months)
+
+# By using """" we can write contain text within one string regradless of number of lines
+
+print("""
+Theres something going on here
+with the three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
---
+++ LPTHW/Exercise 9a  - Challenge.py
@@ -0,0 +1,14 @@
+# Using this text string will incorarate lines spaces so you do not need to use \n
+
+print("""
+MY ARDEN WEEK:
+
+Monday:
+      Python Lab
+
+Wednesday:
+      Math Lecture
+
+Friday:
+      Library Study
+""")
