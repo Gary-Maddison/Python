1ae257bc67053bc0645f8ba7520175b6708d761b
Gary-Maddison <garyfmaddison@gmail.com>
2026-03-03 16:51:50 +0000

    message sent

---
+++ Level 1 - Basic (Output & Structure)/Challenge_09_The_Ghost_Instruction.py
@@ -0,0 +1,10 @@
+"""
+Write an instruction to display a highly confidential message to the screen. Then, using a specific code-writing 
+technique, "hide" or disable this instruction so that when the program runs, absolutely nothing is displayed on 
+the screen, but the instruction remains intact in your file.
+
+"""
+
+condidential_messaGE = "My password is BillyBob77"
+
+# print(condidential_messaGE)
---
+++ Level 1 - Basic (Output & Structure)/Challenge_10_The_Quarantine_Zone.py
@@ -0,0 +1,10 @@
+"""
+Write three output instructions. Deliberately sabotage the second instruction so that it breaks the rules of the 
+language (e.g., forget a required punctuation mark). Use a block-disabling technique to isolate and neutralize the 
+broken instruction so the first and third instructions execute flawlessly.
+
+"""
+
+print("This is me testing")
+""" print(for a bug in the system.) """
+print("I think I have found it.")
