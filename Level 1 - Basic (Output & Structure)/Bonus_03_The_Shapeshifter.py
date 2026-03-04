f32711a1664070fe876cd3f6cfedd0c2052c6c4c
Gary-Maddison <garyfmaddison@gmail.com>
2026-03-04 14:43:25 +0000

    message sent

---
+++ Level 1 - Basic (Output & Structure)/Bonus_03_The_Shapeshifter.py
@@ -0,0 +1,10 @@
+"""
+Store a word in a text container using entirely lowercase letters. Display the word to the screen, but force the output to 
+appear in completely UPPERCASE letters without changing the original text you typed in the 
+container.
+
+"""
+
+user_name = "gary maddison"
+
+print(user_name.upper())
---
+++ Level 2 - Python Fundamentals - Variables, Text Processing and Input Mastery/Challenge_01_The_Quoted_Multi_Line_Message.py
@@ -0,0 +1,10 @@
+"""
+Create a program that outputs a single message to the screen. The message must span exactly two lines. The first 
+line must contain a word wrapped in double quotation marks, and the second line must contain a word 
+indented by a single tab space.
+
+"""
+
+
+
+
