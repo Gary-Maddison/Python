00f75c9415b8d4f7fa6877bdba8c6600442d65b4
Gary-Maddison <garyfmaddison@gmail.com>
2026-03-29 15:03:59 +0100

    Delete Level 1 - Basic (Output & Structure) directory

--- Level 1 - Basic (Output & Structure)/Challenge_01_The_First_Contact.py
+++
@@ -1,7 +0,0 @@
-"""
-Display a word, a number, and a symbol (or emoji). Each of these three items must be displayed on its own separate line.
-"""
-
-print("Hello, World!!!") 
-
-
--- Level 1 - Basic (Output & Structure)/Challenge_04_The_Time_Capsule.py
+++
@@ -1,9 +0,0 @@
-"""
-Create a container specifically meant for whole numbers. Store the year you were born in it. Display 
-the contents of this container to the screen.
-
-"""
-
-year_born = 1977 # Snake_case is used in python
-
-print(f"I was born in {year_born}")
